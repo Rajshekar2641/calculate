889face65b512cceb76dc2be02f576e015ab1929
rajashekar gande <rajshekar2641@gmail.com>
2020-03-02 10:46:24 -0600

    changed script

--- calculate.js
+++ calculate.js
@@ -1,8 +1,6 @@
-function number(){
+function Number(){
     let student = parseFloat(document.getElementById("student").value);
     console.log(typeof student);
     let staff = parseFloat(document.getElementById("staff").value);
-    document.getElementById("total").innerHTML = student+2;
-    
-    return false;
+    document.getElementById("total").innerHTML = student + 2;
 }
