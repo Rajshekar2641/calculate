d6520e9dc56481bba49f1e582cab5bf278fcf178
rajashekar gande <rajshekar2641@gmail.com>
2020-03-02 11:06:26 -0600

    changed styling

--- calculate.js
+++ calculate.js
@@ -1,6 +1,8 @@
+console.log("hi")
+
 function Number(){
     let student = parseFloat(document.getElementById("student").value);
     console.log(typeof student);
-    let staff = parseFloat(document.getElementById("staff").value);
+    // let staff = parseFloat(document.getElementById("staff").value);
     document.getElementById("total").innerHTML = student + 2;
 }
